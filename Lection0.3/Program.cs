ee83bfc27b831317a369707bf5965b2baacae8c7
alexandr <alex98saratovtsev@gmail.com>
2022-08-13 00:05:56 +0400

    Added Lection0.3 and Lection0.4

---
+++ Lection0.3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Поиск максимума из 9 чисел, с применением массива
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {
+    new Random().Next(0, 10),
+    new Random().Next(0, 100),
+    new Random().Next(0, 10),
+    new Random().Next(0, 100),
+    new Random().Next(0, 10),
+    new Random().Next(0, 100),
+    new Random().Next(0, 10),
+    new Random().Next(0, 100),
+    new Random().Next(0, 10),
+};
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine(result);
