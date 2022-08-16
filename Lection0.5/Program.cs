8b8d79ea1e355433eccd8d5655f059181dbdabc8
alexandr <alex98saratovtsev@gmail.com>
2022-08-16 23:22:01 +0400

    Addded 3 Lection

---
+++ Lection0.5/Program.cs
@@ -0,0 +1,40 @@
+﻿void Method1() // Первая группа методов, которая ничего не принимает и не возвращает 
+{
+    System.Console.WriteLine("Автор ...");
+}
+Method1();
+
+// .......................................................................................
+
+void Method2(string msg, int count) // Метод, который принимает два типа значений, но ничего не возвращает
+{
+    int i = 0;
+    while (i < count) {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+// Method2(msg: "Текст", count: 4);
+Method2(count: 4, msg: "Новый текст");
+
+// .......................................................................................
+
+int Method3() // Метод, который ничего не принимает, но возвращает текущий год
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+// .......................................................................................
+
+string Method4(int count, string text) // Метод, который принимает два типа значений, и их возвращает
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++) {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "qwerty");
+Console.WriteLine(res);
---
+++ Lection0.6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Циклы (вывод таблицы умножения на экран)
+
+for (int i = 2; i <= 10; i++) {
+    for (int j = 2; j <= 10; j++) {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
